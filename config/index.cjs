96b1ceef4adc1812b02e85ad1ba923b066057638
zhoushaohia <152966906+zhoushaohia@users.noreply.github.com>
2023-12-05 19:37:46 +0800

    ye

--- config/index.cjs
+++ config/index.cjs
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '情艳',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oYks56osf0TaFdTqO6cHhmk07DV4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'rDJANr13g9_vEzN86e0m14TViyxncGuPuj2Y3CgmCmU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
